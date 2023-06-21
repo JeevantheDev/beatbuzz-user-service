cf17ea56515d56d45794e76f19df383e473c15f1
jeevanDash <jeevan.dash@mbopartners.com>
2023-06-21 12:27:25 +0530

    env separated

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,7 @@
-const isProd = () => process.env.NODE_ENV !== "development";
-
 module.exports.DB_CONFIG = {
-  USER_NAME: isProd() ? process.env.USER_NAME : "postgres",
-  PASSWORD: isProd() ? process.env.PASSWORD : "root",
-  DB: isProd() ? process.env.DB : "beatbuzz-user-service",
-  HOST: isProd() ? process.env.HOST : "localhost",
-  PORT: isProd() ? process.env.DB_PORT : "5432",
+  USER_NAME: process.env.USER_NAME,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  HOST: process.env.HOST,
+  PORT: process.env.DB_PORT,
 };
